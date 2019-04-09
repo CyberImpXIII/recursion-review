28a54a9107493a3a131da5ee2fae34ce79348ab6
CyberImpXIII <jnelsenepstein@gmail.com>
2019-04-08 19:47:44 -0500

    working solution for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+  //input:class name
+  //output:array of HTML elements
   // your code here
+
+  var output = [];
+
+  var recursiveFunction = function(element){
+    if(element.classList.contains(className)){
+      output.push(element);
+    }  
+    var i=0
+    while(i < element.children.length){
+      recursiveFunction(element.children[i]);
+      i+=1;    
+    }
+  };
+  recursiveFunction(document.body);
+  console.log(output);
+  return output;
+
 };
