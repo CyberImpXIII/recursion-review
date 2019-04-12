9a304c9b233b69839f7eb22d5c370078e680a6fb
CyberImpXIII <jnelsenepstein@gmail.com>
2019-04-11 21:22:21 -0500

    added some stuff. messing around with trying to find out which substrings need to be turned to arrays and objects by iterating through and using counters on when the brackets show up and then passing those substrings back through. hopefully some creator functions later

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,7 +1,72 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 
+// object
+//     {}
+//     { members }
+// members
+//     pair
+//     pair , members
+// pair
+//     string : value
+// array
+//     []
+//     [ elements ]
+// elements
+//     value 
+//     value , elements
+// value
+//     string
+//     number
+//     object
+//     array
+//     true
+//     false
+//     null
+
 // but you're not, so you'll write it from scratch:
+
+//look for matching sets of brackets and then when on the innermost set start sending strings where they need to go
+
 var parseJSON = function(json) {
   // your code goes here
+var finder(string){
+  var currentString = ""
+  var curlyBracketCounter = {
+    brackets : false,
+    counter : 0
+  }
+  for(var i =0; i<string.length; i++){
+    if(string[i]==="{" && string[i-1]!== "\\"){
+      curlyBracketCounter.brackets = true;
+      curlyBracketCounter.counter += 1;
+    }
+    if(string[i]==="}" && string[i-1]!== "\\")
+
+  }
+}
+var objectCreate = function(){
+  var results = 
+}
+
+var keyValuePairCreate = function(){
+
+}
+
+var arrayCreate = function(){
+
+}
+
+var stringPass = function(){
+
+}
+
+var numberCreate = function(){
+
+}
+
+var booleanCreate = function(){
+
+}
+
 };
