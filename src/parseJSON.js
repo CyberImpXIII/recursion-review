caf8245503a10f2f5a09f5f029abad23f7a3cb48
CyberImpXIII <jnelsenepstein@gmail.com>
2019-04-15 21:08:18 -0500

    chuggin away at parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -30,23 +30,77 @@
 
 var parseJSON = function(json) {
   // your code goes here
-var finder(string){
+var finder = function(string) {
+
   var currentString = ""
   var curlyBracketCounter = {
     brackets : false,
     counter : 0
   }
+  var arrBracketCounter = {
+    brackets : false,
+    counter : 0
+  }
+
+  var singleQuoteCounter = {
+    exists: false,
+    counter: 0
+  };
+
+  var doubleQuoteCounter = {
+    exists: false,
+    counter: 0
+  }
   for(var i =0; i<string.length; i++){
     if(string[i]==="{" && string[i-1]!== "\\"){
       curlyBracketCounter.brackets = true;
-      curlyBracketCounter.counter += 1;
+      curlyBracketCounter.counter ++;
     }
-    if(string[i]==="}" && string[i-1]!== "\\")
+    if(string[i]==="}" && string[i-1]!== "\\"){
+      curlyBracketCounter.counter --;
+    }
+    if(string[i]==="[" && string[i-1]!== "\\"){
+      arrBracketCounter.brackets = true;
+      arrBracketCounter.counter ++;
+    }
+    if(string[i]==="]" && string[i-1]!== "\\"){
+      arrBracketCounter.counter --;
+    }
+    if(string[i]=== "\"" && string[i-1]!== "\\"){
+      doubleQuoteCounter.exists = true;
+      doubleQuoteCounter.counter ++;
+    }
+    if(string[i]=== "\'" && string[i-1]!== "\\"){
+      singleQuoteCounter.exists = true;
+      singleQuoteCounter.counter ++;
+    }
 
+    currentString += string[i];
+
+    if(singleQuoteCounter.counter === 2){
+      return stringPass(currentString);
+    }
+    if(singleQuoteCounter.counter === 2){
+      return stringPass(currentString);
+    }
+    if(curlyBracketCounter.brackets === 0 && curlyBracketCounter.counter === true){
+      arrayCreate(currentString);
+    }
+    if(arrBracketCounter.brackets === 0 && arrBracketCounter.counter === true){
+      objectCreate(currentString);
+    }
+    if(typeOf (Number(currentString)) === "number"){
+
+    }
+    if((currentString.includes("true") || currentString.includes("false") ) && doubleQuoteCounter.exists === false && singleQuoteCounter.exists === false){
+      booleanCreate(currentString)
+    }
   }
 }
-var objectCreate = function(){
-  var results = 
+
+
+var objectCreate = function(currentString){
+  var results = Object(currentString.split("").pop().shift().join(""))
 }
 
 var keyValuePairCreate = function(){
@@ -57,16 +111,16 @@ var arrayCreate = function(){
 
 }
 
-var stringPass = function(){
-
+var stringPass = function(currentString){
+  return String(currentString.split("").pop().shift().join(""));
 }
 
 var numberCreate = function(){
 
 }
 
-var booleanCreate = function(){
-
+var booleanCreate = function(currentString){
+  return currentString.includes("true");
 }
 
 };
